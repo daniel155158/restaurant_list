169c3a79684baef9fa0b7e24dead9a23fde94713
daniel155158 <daniel155158@gmail.com>
2022-12-18 11:52:49 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require('./config/mongoose')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const usePassport = require('./config/passport')
 const routes = require('./routes')
 const port = 3000
 
@@ -22,6 +23,7 @@ app.use(session({
   saveUninitialized: true
 }))
 app.use(methodOverride('_method'))
+usePassport(app) // 呼叫Passport函式並傳入app
 app.use(routes)
 
 app.listen(port, () => {
