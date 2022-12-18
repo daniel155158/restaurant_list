b632673970a54affcee1949412eaf20a51504eac
daniel155158 <daniel155158@gmail.com>
2022-12-18 13:50:17 +0800

    feat: add isAuthenticated to display different view template

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ app.use(session({
 }))
 app.use(methodOverride('_method'))
 usePassport(app) // 呼叫Passport函式並傳入app
+app.use((req, res, next) => {
+  console.log(req.user)
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
