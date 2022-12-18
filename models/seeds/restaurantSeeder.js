368f69686ab50390a249f776933e70217b293cd3
daniel155158 <daniel155158@gmail.com>
2022-12-18 22:56:58 +0800

    feat: modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,22 +1,46 @@
 const Restaurant = require('../restaurant')
-const restaurants = require('./restaurant.json').results
+const User = require('../user')
+const restaurantList = require('./restaurant.json').results
 const db = require('../../config/mongoose')
+const bcrypt = require('bcryptjs')
 
-db.once('open', () => {
-  restaurants.forEach(function (restaurant) {
-    Restaurant.create({
-      name: restaurant.name,
-      name_en: restaurant.name_en,
-      category: restaurant.category,
-      image: restaurant.image,
-      location: restaurant.location,
-      phone: restaurant.phone,
-      google_map: restaurant.google_map,
-      rating: Number(restaurant.rating),
-      description: restaurant.description
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const SEED_USERS = [
+  {
+    name: 'USER1',
+    email: 'user1@example.com',
+    password: '12345678',
+    restaurantIndex: [0, 1, 2]
+  },
+  {
+    name: 'USER2',
+    email: 'user2@example.com',
+    password: '12345678',
+    restaurantIndex: [3, 4, 5]
+  }
+]
+
+db.once('open', async () => {
+  await Promise.all(SEED_USERS.map(async (user) => {
+    const { name, email, password, restaurantIndex } = user
+    const salt = await (bcrypt.genSalt(10))
+    const hash = await (bcrypt.hash(password, salt))
+    const createdUser = await User.create({
+      name,
+      email,
+      password: hash
     })
-  })
-  console.log('done!')
+    const restaurant = restaurantIndex.map(index => {
+      restaurantList[index].userID = createdUser._id
+      return restaurantList[index]
+    })
+    await Restaurant.create(restaurant)
+  }))
+  console.log('done.')
+  process.exit()
 })
 
 //助教提供的insertMany寫法
@@ -24,5 +48,4 @@ db.once('open', () => {
 //   Restaurant.insertMany(restaurants)
 //     .then(() => console.log('done!'))
 //     .catch(error => console.log(error))
-
 // })
